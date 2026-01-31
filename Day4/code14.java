6ca5fcfdbb9e790d830bd4dbc4da756f0ae0152f
Varshini S <varshinigowdasv@gmail.com>
2026-01-31 09:35:37 +0530

    Add files via upload

---
+++ Day4/code14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class code14{
+    public static void main(String[] args){
+Scanner va = new Scanner(System.in);
+System.out.print("Enter integer N:");
+int n;
+n=va.nextInt();
+int sum;int i;
+sum=0;
+for(i=0;i<=n;i++){
+
+    sum=sum+i;
+}
+
+    System.out.println(sum);
+}
+
+
+
+
+    }
