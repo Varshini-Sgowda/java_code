b6515541d6f018c3711a70fcb2684a0d7e142d68
Varshini S <varshinigowdasv@gmail.com>
2026-01-31 09:16:12 +0530

    Delete code3.java

--- code3.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class code3{
-   public static void main(String[] args){
-    double p=2.35;
-    int q= (int)p;
-    System.out.println(p);
-    System.out.println(q);
-   }   
-}
