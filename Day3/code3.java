cf96ea05f714ac01bc0c63b542aa45cc83a88f8c
Varshini S <varshinigowdasv@gmail.com>
2026-01-31 09:15:03 +0530

    Add files via upload

---
+++ Day3/code3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class code3{
+   public static void main(String[] args){
+    double p=2.35;
+    int q= (int)p;
+    System.out.println(p);
+    System.out.println(q);
+   }   
+}
---
+++ Day3/code4.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class code4{
+    public static void main(String[] args){
+
+        char x='A';
+        System.out.println((double-)x);
+    }
+}
